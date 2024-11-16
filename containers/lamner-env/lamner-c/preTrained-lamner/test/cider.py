c75702d631ccfdc59b9e5a195a1331660ff7c9ae
Eray Erer <eray.erer@hotmail.com>
2024-11-17 00:46:28 +0300

    cider test

---
+++ containers/lamner-env/lamner-c/preTrained-lamner/test/cider.py
@@ -0,0 +1,19 @@
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.translate.bleu_score import sentence_bleu, SmoothingFunction
+import math
+from cidereval import cider, ciderD
+
+predicted_file_name = "test-predictions.out.txt"
+ref_file_name = "test-trgs.given.txt"
+chencherry = SmoothingFunction()
+with open(predicted_file_name) as f1:
+    #predicted_lines = [line.rstrip('\n.') for line in f1]
+    predicted_lines = f1.readlines()
+with open(ref_file_name) as f2:
+    #ref_lines = [line.rstrip('\n.') for line in f2]
+    ref_lines = f2.readlines()
+
+cider(predictions=predicted_lines, references=ref_lines)
+
+print(cider["avg_score"])
