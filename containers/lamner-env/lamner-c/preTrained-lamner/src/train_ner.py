fba1732af5c4b46660a9b65ea923c6b7e275ff64
Eray Erer <eray.erer@hotmail.com>
2024-12-09 18:10:55 +0300

    help

--- containers/lamner-env/lamner-c/preTrained-lamner/src/train_ner.py
+++ containers/lamner-env/lamner-c/preTrained-lamner/src/train_ner.py
@@ -256,14 +256,14 @@ def train_ner_model(args):
   tag_type = 'ner'
   tag_dictionary = corpus.make_tag_dictionary(tag_type=tag_type)
   if args.codebert:
-    embedding_types: List[TokenEmbeddings] = [
-                                             TransformerDocumentEmbeddings('microsoft/codebert-base'),
-                                             FlairEmbeddings('resources/taggers/code_language_model/best-lm.pt'),
-                                              ]
+    embedding_types = [
+                         TransformerDocumentEmbeddings('microsoft/codebert-base'),
+                         FlairEmbeddings('resources/taggers/code_language_model/best-lm.pt'),
+                          ]
   else:
-    embedding_types: List[TokenEmbeddings] = [
-                                             FlairEmbeddings('resources/taggers/code_language_model/best-lm.pt'),
-                                              ]
+    embedding_types = [
+                         FlairEmbeddings('resources/taggers/code_language_model/best-lm.pt'),
+                          ]
   embeddings: StackedEmbeddings = StackedEmbeddings(embeddings=embedding_types)
   tagger: SequenceTagger = SequenceTagger(hidden_size=int(args.embedding_size/4),
                                         embeddings=embeddings,
